168ee853b35e9ac4d4a86214aae9f8c1b7e557e8
Your Name <your@email.com>
2025-07-17 13:53:51 +0000

    asd

--- Social-media-server/constants/config.js
+++ Social-media-server/constants/config.js
@@ -1,9 +1,9 @@
 const corsOptions = {
   origin: [
     process.env.CLIENT_URL,
-    "https://social-media-adi.vercel.app",
-    "http://localhost:5173",
-    "https://refactored-eureka-g46564rq6qpwcvj6p-5173.app.github.dev"
+    // "https://social-media-adi.vercel.app",
+    // "http://localhost:5173",
+    // Add other allowed origins here
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
