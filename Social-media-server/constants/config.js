3aecc0b1a83fec3d7aa7b0df9d7dc9e5c67ae27e
Aditya Gautam <cs23b2037@iiitdm.ac.in>
2025-07-18 21:32:59 +0000

    fixed the envs

--- Social-media-server/constants/config.js
+++ Social-media-server/constants/config.js
@@ -1,8 +1,8 @@
 const corsOptions = {
   origin: [
     process.env.CLIENT_URL,
-    "https://laughing-potato-69wrw95g56g424v7v-5173.app.github.dev",
-    // "https://social-media-adi.vercel.app"
+    // "https://laughing-potato-69wrw95g56g424v7v-5173.app.github.dev",
+    "https://social-media-adi.vercel.app"
     // "http://localhost:5173",
     // Add other allowed origins here
   ],
